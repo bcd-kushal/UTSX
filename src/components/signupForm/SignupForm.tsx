66d2b932aa4cefe99de85ce61559d0e8a6dde182
Kushal <dev@kushalkumarsaha.com>
2024-05-11 23:57:30 +0530

    UPDATE: signupForm

--- src/components/signupForm/SignupForm.tsx
+++ src/components/signupForm/SignupForm.tsx
@@ -1,11 +1,15 @@
 import { RegistrationForm } from "./logic/SignupFormLogic"
+import { SignupRegistrationProps } from "./utils/interfaces"
 
 
 export default function SignupForm() {
-
+    const RegistrationData: SignupRegistrationProps = {
+        title: "Register",
+        description: "Register to proceed"
+    }
     return (
-        <section className="flex justify-center px-[24px] items-center min-h-[calc(100vh-118px)] md:min-h-[calc(100vh-138px)] md:px-[30px] md:mt-[30px]">
-            <RegistrationForm />
+        <section className="flex justify-center items-center min-h-full">
+            <RegistrationForm props={RegistrationData}/>
         </section>
     )
 }
