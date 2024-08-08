56fbb896e0b1b27de59e99309638f7eb3ff808f7
bcd-kushal <dev@kushalkumarsaha.com>
2024-08-08 07:18:31 +0530

    ADD: Toggle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
-import SearchBar from "@/components/utsx/searchBar/SearchBar"
-import { SearchBarProps, SearchTagsType } from "@/components/utsx/searchBar/utils/types"
-import { Link2Icon, ReaderIcon, CardStackPlusIcon, ArrowTopRightIcon, ImageIcon, ResumeIcon } from "@radix-ui/react-icons"
+'use client'
+import Toggle from "@/components/utsx/toggle/Toggle"
+import { useState } from "react"
 
 export default function Home() {
-	const searchProps: SearchBarProps = {
-		placeholder: " "
-	}
-
-	const tags: SearchTagsType = [
-        { label:"Has attachment", link:"#", svg:<Link2Icon height={12} width={12}/> },
-        { label:"Has files", link:"#", svg:<ReaderIcon height={12} width={12}/> },
-        { label:"Has folders", link:"#", svg:<CardStackPlusIcon height={12} width={12}/> },
-        { label:"Has links", link:"#", svg:<ArrowTopRightIcon height={12} width={12}/> },
-        { label:"Pictures", link:"#", svg:<ImageIcon height={12} width={12}/> },
-        { label:"Videos", link:"#", svg:<ResumeIcon height={12} width={12}/> },
-    ]
-
+	const [toggled, setToggled] = useState<boolean>(false)
 
 	return (
-		<div className="w-screen px-20 py-5"> 
-			<SearchBar props={searchProps} searchTags={tags} />
-		</div>
+		<Toggle value={toggled} setValue={setToggled} activeColor="bg-red-400" activeBGColor="bg-red-300/40" />
 	)
 }
